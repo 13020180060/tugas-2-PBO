e7e4c8465761c89bbb560447deb959d40a1dcacb
13020180060 <61998954+13020180060@users.noreply.github.com>
2020-03-16 07:23:48 -0700

    Add files via upload

---
+++ tugas PBO1/uji.java
@@ -0,0 +1,19 @@
+/*Nama     : Ita masita
+  kelas    : b1 TI
+  hari/tgl : senin, 16 mar 2020
+*/
+public class uji{
+ 	
+	public static void main(String[] args){
+	 String nim      ="1302080060";
+	 String nama     ="ita masita";
+	 String jurusan  ="Teknik informatika";
+	 String fakultas ="ilmu komputer";
+	System.out.println("nim saya : " + nim);
+	System.out.println("nama saya : " + nama);
+	System.out.println("jurusan saya : " + jurusan);
+	System.out.println("fakultas saya : " + fakultas);
+}
+}
+
+	
