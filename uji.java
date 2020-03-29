bbcd1fce38d5f2e917a04ee325aca6557cf4594f
13020180060 <61998954+13020180060@users.noreply.github.com>
2020-03-29 02:43:21 -0700

    Add files via upload

---
+++ uji.java
@@ -0,0 +1,22 @@
+import.java.util;
+public class uji{
+ 	
+public static void main(String[] args){
+Scanner scan=new Scanner(System.in);
+String nim, nama, jurusan, fakultas;
+System.out.print("nim : ");
+nim = input.nextLine();
+System.out.print("nama : ");
+nama = input.nextLine();
+System.out.print("jurusan : ");
+jurusan = input.nextLine();
+System.out.print("fakultas : ");
+fakultas = input.nextLine();
+	System.out.println("nim saya : " + nim);
+	System.out.println("nama saya : " + nama);
+	System.out.println("jurusan saya : " + jurusan);
+	System.out.println("fakultas saya : " + fakultas);
+}
+}
+
+	
